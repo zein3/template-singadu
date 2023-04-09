79177b9063437f5b8ec21796d0bcf6efc0caefdb
Zein Haddad <zeinhaddad02@gmail.com>
2023-04-09 19:29:04 +0700

    improved dark theme system

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,15 @@
 const toggleTheme = () => {
   document.body.classList.toggle('dark');
+
+  if (document.getElementsByClassName('dark').length !== 0) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark');
+  }
+});
