05e87f10c1a30196a09eee84e146c921db2b24e6
Zein Haddad <zeinhaddad02@gmail.com>
2023-04-10 13:22:47 +0700

    added theme toggle switch

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -9,7 +9,14 @@ const toggleTheme = () => {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+  const themeToggler = document.getElementById('theme-toggler');
+  if (themeToggler != null) {
+    themeToggler.addEventListener('click', toggleTheme);
+    themeToggler.checked = !(localStorage.getItem('theme') === 'dark');
+  }
+
   if (localStorage.getItem('theme') === 'dark') {
     document.body.classList.add('dark');
+  } else {
   }
 });
