8772e4762e011c4e7b44bd16e0a070ce8a090c3d
Zein Haddad <zeinhaddad02@gmail.com>
2023-04-12 08:26:48 +0700

    remove modal made

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -12,3 +12,8 @@ const openDetailModal = (detail) => {
   document.querySelector("#detail").innerText = detail;
   document.querySelector("#detail-modal").classList.add('open');
 }
+
+const openDeleteModal = (id) => {
+  document.querySelector('#delete-id').value = id;
+  document.querySelector('#delete-modal').classList.add('open');
+}
